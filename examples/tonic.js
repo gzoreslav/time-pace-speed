fe91612930bee0017b95bcea540dd3bb54ed60e4
Zoreslav Goral <Zoreslav.Goral@securenettech.com>
2016-04-04 18:27:05 +0300

    update version, fix tonic example

--- examples/tonic.js
+++ examples/tonic.js
@@ -3,22 +3,21 @@
 var tps = require("time-pace-speed");
 
 /* GENERAL EXAMPLE */
-var i = tps.interval(21.6, '01:13:45.120');
-var speed = i.speed_km_per_hour();
-var pace = i.pace_per_km('mm:ss.SSS');
-console.log('User run distance of 21.6km in 01:13:45.120');
+var i = tps.interval(21600, '01:13:45.120');
+var speed = i.speed.km_h();
+var pace = i.pace.per_km().format('mm:ss.SSS');
+console.log(`User run ${i.dist.km()} in ${i.time.format()}`);
 console.log(`speed ${speed} km/h, pace ${pace}`);
 
 /* DISTANCES */
-var d1 = tps.dist(1);
+var d1 = tps.dist(1, 'km');
 console.log(`1 km = ${d1.km()} km`);
 console.log(`1 km = ${d1.m()} m`);
 console.log(`1 km = ${d1.miles()} miles`);
-
 var d2 = tps.dist(1, 'mile');
 console.log(`1 mile = ${d2.km()} km`);
 
-/* TIMES */
+/* DURATION (TIME) */
 console.log(tps.time('01:13:45.120').format());
 console.log(tps.time('00:13:45.000').format('mm:ss'));
 console.log(tps.time('13:45', 'ss:mm').format('HH:mm:ss'));
